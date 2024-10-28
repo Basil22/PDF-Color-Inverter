b4947c1ca8492611a2de3859cf5d737f7ac63dfc
Basil <basilrasheed24@gmail.com>
2024-10-28 15:49:26 +0530

    Add files

---
+++ pdf-Inverter-backend/src/main/java/com/pdf_inverter/PdfInverterApplication.java
@@ -0,0 +1,13 @@
+package com.pdf_inverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfInverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfInverterApplication.class, args);
+	}
+
+}
